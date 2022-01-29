cdc97537a692a4917d19e7ad20c9b90bad90944a
zhuyan129 <zhuyan@stu.pku.edu.cn>
2022-01-29 21:03:47 +0800

    add login

--- front-end/pages/login/login.vue
+++ front-end/pages/login/login.vue
@@ -1,6 +1,11 @@
 <template>
-	<view>
-		login
+	<view class="login-container">
+		<!-- 提示登录的图标 -->
+		<uni-icons type="contact-filled" size="100" color="#AFAFAF"></uni-icons>
+		<!-- 登录按钮 -->
+		<button class="btn-login" type="default">一键登录</button>
+		<!-- 登录提示 -->
+		<view class="tips-text">登录后尽享更多权益</view>
 	</view>
 </template>
 
@@ -15,5 +20,40 @@
 </script>
 
 <style lang="scss">
+.login-container{
+	//登录盒子的样式
+	height: 750rpx;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background-color: #f8f8f8;
+	position: relative;
+	overflow: hidden;
+	
+	&::after{
+		content: ' ';
+		display: block;
+		width: 100%;
+		height: 40px;
+		background-color: white;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		border-radius: 100%;
+		transform: translateY(50%);
+	}
+}
 
+.btn-login[type="default"]{
+	color: white;
+	background-color: #4285F4;
+	width: 90%;
+	margin: 15px 0;
+}
+
+.tips-text{
+	font-size: 12px;
+	color: gray;
+}
 </style>
